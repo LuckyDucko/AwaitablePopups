7a169d9d91716b511c6e47e29af9e59465692df9
Tyson Elliot Hooker <tyson@logchecker.com.au>
2020-06-30 16:07:21 +1000

    revert to list for time being

--- AwaitablePopups/Interfaces/ILoaderViewModel.cs
+++ AwaitablePopups/Interfaces/ILoaderViewModel.cs
@@ -4,12 +4,12 @@ using Xamarin.Forms;
 
 namespace AwaitablePopups.Interfaces
 {
-	public interface ILoaderViewModel
-	{
-		Color LoaderColour { get; set; }
-		Color TextColour { get; set; }
-		IEnumerable<string> ReasonsForLoader { get; set; }
-		int MillisecondsBetweenReasonSwitch { get; set; }
-		void SafeCloseModal();
-	}
+    public interface ILoaderViewModel
+    {
+        Color LoaderColour { get; set; }
+        Color TextColour { get; set; }
+        List<string> ReasonsForLoader { get; set; }
+        int MillisecondsBetweenReasonSwitch { get; set; }
+        void SafeCloseModal();
+    }
 }
